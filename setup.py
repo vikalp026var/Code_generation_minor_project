84d974687693eca1cbeeea100262849226f375e0
vikalp026var <vikalp026varshney@gmail.com>
2024-02-29 23:39:23 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+__name__="Vikalp & Mohd Arasalan"
+__version__='0.0.1'
+project_description="Code generation on NLP Based Programming."
+
+setup(
+     name=__name__,
+     version=__version__,
+     author_email='vikalp026varshney@gmail.com',
+     description=project_description,
+     packages=find_packages(),
+     include_dirs=[]
+)
